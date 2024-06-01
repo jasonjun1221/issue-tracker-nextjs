ee4888a5c2de3c1fb12bc5ff26b622055b5be6e7
Jason <jasonchong1221@hotmail.com>
2024-06-01 20:19:47 +0800

    Build an API for updating issue

---
+++ app/api/issues/[id]/route.ts
@@ -0,0 +1,36 @@
+import { issueSchema } from "@/app/schemas/issueSchema";
+import prisma from "@/prisma/db";
+import { NextRequest, NextResponse } from "next/server";
+
+interface Params {
+  params: { id: string };
+}
+
+export async function PATCH(request: NextRequest, { params }: Params) {
+  const body = await request.json();
+
+  const validation = issueSchema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json({ errors: validation.error }, { status: 400 });
+  }
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: params.id,
+    },
+  });
+  if (!issue) {
+    return NextResponse.json({ error: "Issue not found" }, { status: 404 });
+  }
+
+  const updatedIssue = await prisma.issue.update({
+    where: {
+      id: issue.id,
+    },
+    data: {
+      title: validation.data.title,
+      description: validation.data.description,
+    },
+  });
+  return NextResponse.json(updatedIssue);
+}
