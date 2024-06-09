517e8111158ba93c5aa5feb987f86e0d1a67eb0f
Jason <jasonchong1221@hotmail.com>
2024-06-09 13:04:24 +0800

    Build issuesSummary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-
+import prisma from "@/prisma/db";
+import IssuesSummary from "./IssuesSummary";
 import LatestIssues from "./LatestIssues";
 
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: {
+      status: "OPEN",
+    },
+  });
 
-export default function Home() {
-  return <LatestIssues />;
+  const inProgress = await prisma.issue.count({
+    where: {
+      status: "IN_PROGRESS",
+    },
+  });
+
+  const closed = await prisma.issue.count({
+    where: {
+      status: "CLOSED",
+    },
+  });
+
+  return <IssuesSummary open={open} inProgress={inProgress} closed={closed} />;
 }
